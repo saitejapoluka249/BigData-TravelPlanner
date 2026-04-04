7b8d332d42d506703f064eab8dd5f2986c671c65
emersonliu <emli8374@colorado.edu>
2026-04-03 22:47:27 -0600

    added fastapi param checking here rather than in service call

--- backend/app/api/v1/endpoints/weather.py
+++ backend/app/api/v1/endpoints/weather.py
@@ -2,16 +2,17 @@ from fastapi import APIRouter, HTTPException, Query
 from app.services.weather_service import weather_service
 from app.schemas.weather import WeatherSummary
 from fastapi_cache.decorator import cache
+from datetime import date
 
 router = APIRouter()
 
 @router.get("/forecast", response_model=WeatherSummary)
 @cache(expire=3600)
 async def get_trip_weather(
-    lat: float = Query(..., description="Destination Latitude"),
-    lon: float = Query(..., description="Destination Longitude"),
-    check_in_date: str = Query(..., description="Arrival Date YYYY-MM-DD"),
-    check_out_date: str = Query(..., description="Departure Date YYYY-MM-DD")
+    lat: float = Query(..., ge= -90, le= 90, description="Latitude must be between -90 and 90"),
+    lon: float = Query(..., ge= -180, le= 180, description="Longitude must be between -180 and 180"),
+    check_in_date: date = Query(...),
+    check_out_date: date = Query(...)
 ):
     """
     Fetches the weather for the specific dates of the trip.
@@ -19,7 +20,7 @@ async def get_trip_weather(
     """
     result = await weather_service.get_weather_for_trip(
         lat=lat, lon=lon, 
-        check_in_date=check_in_date, check_out_date=check_out_date
+        check_in_date=str(check_in_date), check_out_date=str(check_out_date)
     )
     
     if isinstance(result, dict) and "error" in result:
